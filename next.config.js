610b2a3dba87dac770c0f5ded9284e917089ae17
Vijay Jayaram <vijay.jayaram@mixpanel.com>
2023-04-09 18:05:29 -0700

    stuff

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  staticImage: true,
+  latex: true,
+  defaultShowCopyCode: true,
+})
+
+module.exports = withNextra()
---
+++ theme.config.tsx
@@ -0,0 +1,43 @@
+import cn from 'clsx'
+import {FC, ReactNode, React} from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+function renderComponent<T>(
+  ComponentOrNode: FC<T> | ReactNode,
+  props?: T
+) {
+  if (!ComponentOrNode) return null
+  if (typeof ComponentOrNode !== 'function') return ComponentOrNode
+  return <ComponentOrNode {...props} />
+}
+
+const config: DocsThemeConfig = {
+  logo: <span>Mixpanel Documentation</span>,
+  project: {
+    link: 'https://github.com/ranic/documentation-starter-kit'
+  },
+  staticImage: true,
+  docsRepositoryBase: 'https://github.com/ranic/documentation-starter-kit/tree/main',
+  sidebar: {
+    toggleButton: true,
+    titleComponent({ title, type, route }) {
+      if (type !== 'separator' && route.match(/\//g) && route.match(/\//g).length == 1) {
+      	return (
+		    <li
+		      className={cn(
+		        '[word-break:break-word]',
+		        title
+		          ? 'nx-mt-5 nx-mb-2 nx-px-2 nx-py-1.5 nx-text-sm nx-font-semibold nx-text-gray-900 first:nx-mt-0 dark:nx-text-gray-100'
+		          : 'nx-my-4'
+		      )}
+		    >
+            {title}
+		    </li>
+		  )
+      }
+      return <>{title}</>
+	},
+  }
+}
+
+export default config
