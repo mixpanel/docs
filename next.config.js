1855451403955a051ad9494abe1d1611b9d2894c
Vijay Jayaram <vijay.jayaram@mixpanel.com>
2023-04-15 16:54:57 -0700

    redirect + getting starteD

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const withNextra = require('nextra')({
   staticImage: true,
   latex: true,
   defaultShowCopyCode: true,
+  redirects: () => {
+    return [
+      {
+        source: "/",
+        destination: "/getting-started/what-is-mixpanel",
+        statusCode: 301,
+      },
+    ]
+  }
 })
 
 module.exports = withNextra()
