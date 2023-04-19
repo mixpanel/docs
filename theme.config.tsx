5abc9b0d2ef559c8930297203aeaa1efa70be914
Vijay Jayaram <ranic@users.noreply.github.com>
2023-04-18 18:17:43 -0700

    stuff (#29)

--- theme.config.tsx
+++ theme.config.tsx
@@ -38,30 +38,6 @@ const config: DocsThemeConfig = {
   primaryHue: { dark: 256, light: 259 },
   sidebar: {
     toggleButton: true,
-
-    // Some magic to make top-level pages look like Separators
-    // We should probably redesign this at some point
-    titleComponent({ title, type, route }) {
-      if (
-        type !== "separator" &&
-        route.match(/\//g) &&
-        route.match(/\//g).length == 1
-      ) {
-        return (
-          <li
-            className={cn(
-              "[word-break:break-word]",
-              title
-                ? "nx-mt-5 nx-mb-2 nx-px-2 nx-py-1.5 nx-text-sm nx-font-semibold nx-text-gray-900 first:nx-mt-0 dark:nx-text-gray-100"
-                : "nx-my-4"
-            )}
-          >
-            {title}
-          </li>
-        );
-      }
-      return <>{title}</>;
-    },
   },
 };
 
