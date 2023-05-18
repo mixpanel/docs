5e7805a1fb328216329af6a5bd7f2d27064d1034
Micah Allen <micah.allen@mixpanel.com>
2023-05-18 14:52:56 -0400

    Add Mixpanel Docs to SEO Title

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
-import cn from "clsx";
-import { FC, ReactNode } from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import {FC, ReactNode} from "react";
+import {DocsThemeConfig} from "nextra-theme-docs";
 import MixpanelLogoWordmark from "./components/svg/MixpanelLogoWordmark";
 
 function renderComponent<T>(ComponentOrNode: FC<T> | ReactNode, props?: T) {
@@ -37,6 +36,9 @@ const config: DocsThemeConfig = {
   },
   logo: <MixpanelLogoWordmark width={125} />,
   logoLink: "https://mixpanel.com/home",
+  useNextSeoProps: () => ({
+    titleTemplate: '%s - Mixpanel Docs'
+  }),
   project: {
     link: "https://github.com/mixpanel/docs",
   },
