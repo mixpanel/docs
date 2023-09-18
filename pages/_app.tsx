1b98a4d988a4fec26b84e2c23ecbd4e19bbbdd52
RhoEun Song <104846040+mixpanel-rhoeun@users.noreply.github.com>
2023-09-18 23:39:10 +0000

    removed sprig related codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,15 @@
 import "./globals.css";
 import "./overrides.scss";
-import React from 'react';
-import {useEffect} from 'react';
-import { insertGTMScriptTags } from '../components/GTMScripts';
-import type { AppProps } from 'next/app'
-
+import React from "react";
+import { useEffect } from "react";
+import { insertGTMScriptTags } from "../components/GTMScripts";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-        insertGTMScriptTags();
-        // @ts-ignore
-        if (typeof Sprig !== `undefined`) {
-          // if Sprig loaded in browser
-          // @ts-ignore
-          Sprig('track', 'helpDocViewed')
-        }
-    }, []);
-   
-  return <Component {...pageProps} />
+    insertGTMScriptTags();
+    // @ts-ignore
+  }, []);
+
+  return <Component {...pageProps} />;
 }
