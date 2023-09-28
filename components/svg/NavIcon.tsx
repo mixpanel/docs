c787c99f173e080beca482d0bd3966da6bcc00db
Tiffany Qi <tiffany19qi@gmail.com>
2023-09-28 14:52:19 -0700

    move out of icons out of theme

--- components/svg/NavIcon.tsx
+++ components/svg/NavIcon.tsx
@@ -1,4 +1,4 @@
-export function IntroIcon() {
+function IntroIcon() {
     return (
         <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9563 4.41347C14.3186 4.04528 13.5031 4.26378 13.1349 4.9015L12.4844 6.02816C12.8305 5.39475 12.6091 4.59759 11.9807 4.23476C11.343 3.86657 10.5275 4.08507 10.1593 4.72279L6.49254 11.0739L6.49213 11.0736L6.29604 9.44604C6.18413 8.51719 5.34043 7.85494 4.41159 7.96684C3.5042 8.07617 2.84713 8.88558 2.92666 9.79607L3.34202 14.5513C3.4361 15.6283 4.04975 16.5917 4.98605 17.1323L9.26563 19.6031C11.1788 20.7077 13.6252 20.0522 14.7297 18.139L15.7297 16.407L15.7297 16.4069L18.5244 11.5664C18.8926 10.9287 18.6741 10.1132 18.0364 9.74501C17.3994 9.37729 16.5852 9.59477 16.2164 10.2306L17.0869 8.72287C17.4551 8.08515 17.2366 7.2697 16.5989 6.90151C15.9667 6.5365 15.1598 6.74808 14.7871 7.3731L15.4443 6.23484C15.8125 5.59711 15.594 4.78166 14.9563 4.41347Z" fill="#B094FF"/>
@@ -10,7 +10,7 @@ export function IntroIcon() {
     )
 }
 
-export function DataInIcon() {
+function DataInIcon() {
     return (
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M11 0C11.5523 0 12 0.447715 12 1V8H15C15.5523 8 16 8.44771 16 9V15C16 15.5523 15.5523 16 15 16H9C8.44772 16 8 15.5523 8 15V12H1C0.447716 12 0 11.5523 0 11V1C0 0.447715 0.447715 0 1 0H11Z" fill="#B094FF"/>
@@ -21,7 +21,7 @@ export function DataInIcon() {
     )
 }
 
-export function AnalysisIcon() {
+function AnalysisIcon() {
     return (
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_1487_13879)">
@@ -40,7 +40,7 @@ export function AnalysisIcon() {
     )
 }
 
-export function AdminIcon() {
+function AdminIcon() {
     return (
         <svg width="14" height="17" viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M4 7.66667V3.66666C4 2.1939 5.19391 1 6.66667 1V1C8.13943 1 9.33333 2.19391 9.33333 3.66667V7.66667" stroke="#7856FF" stroke-linejoin="round"/>
@@ -51,7 +51,7 @@ export function AdminIcon() {
     )
 }
 
-export function DataOutIcon() {
+function DataOutIcon() {
     return (
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M5 0C4.44772 0 4 0.447715 4 1V8H1C0.447715 8 0 8.44771 0 9V15C0 15.5523 0.447715 16 1 16H7C7.55228 16 8 15.5523 8 15V12H15C15.5523 12 16 11.5523 16 11V1C16 0.447715 15.5523 0 15 0H5Z" fill="#B094FF"/>
@@ -62,7 +62,7 @@ export function DataOutIcon() {
     )
 }
 
-export function SupportIcon() {
+function SupportIcon() {
     return (
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6.08579 14.0858L7.29289 15.2929C7.68342 15.6834 8.31658 15.6834 8.70711 15.2929L9.91421 14.0858C10.2893 13.7107 10.798 13.5 11.3284 13.5H14C15.1046 13.5 16 12.6046 16 11.5V2C16 0.895431 15.1046 0 14 0H2C0.895431 0 0 0.89543 0 2V11.5C0 12.6046 0.895431 13.5 2 13.5H4.67157C5.20201 13.5 5.71071 13.7107 6.08579 14.0858Z" fill="#B094FF"/>
@@ -72,3 +72,12 @@ export function SupportIcon() {
         </svg>
     )
 }
+
+export {
+    IntroIcon,
+    DataInIcon,
+    AnalysisIcon,
+    AdminIcon,
+    DataOutIcon,
+    SupportIcon,
+};
