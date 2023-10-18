35faaf177d49cf5221086c0867fceac1581d3c93
Tiffany Qi <tiffany19qi@gmail.com>
2023-10-18 12:43:33 -0700

    add url param to focus on tab in mobile

---
+++ utils/mobileUrlParams.ts
@@ -0,0 +1,8 @@
+export const urlToItemsMap = {
+    'reactnative': `React Native`,
+    'flutter': `Flutter`,
+    'ios': `iOS (Objective-C)`,
+    'swift': `iOS (Swift)`,
+    'android': `Android`,
+    'unity': `Unity`,
+}
