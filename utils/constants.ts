8209c59585e0cd4a188dc3cff22d11890c301d9d
Tiffany Qi <tiffany19qi@gmail.com>
2023-10-18 13:47:16 -0700

    add to ruby

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,16 @@
-export const urlToItemsMap = {
+export const mobileItems = {
     'reactnative': `React Native`,
     'flutter': `Flutter`,
     'ios': `iOS (Objective-C)`,
     'swift': `iOS (Swift)`,
     'android': `Android`,
     'unity': `Unity`,
+}
+
+export const serverItems = {
+    'python': `Python`,
+    'node': `Node`,
+    'go': `Go`,
+    'ruby': `Ruby`,
+    'java': `Java`,
 }
