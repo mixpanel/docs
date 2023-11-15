678d2db65a9ee531f8aeb26926a294d6ef671ebc
Tiffany Qi <tiffany19qi@gmail.com>
2023-11-14 16:56:17 -0800

    add feedback show

--- components/FeedbackCollector/FeedbackCollector.tsx
+++ components/FeedbackCollector/FeedbackCollector.tsx
@@ -1,4 +1,6 @@
 import clsx from "clsx";
+import { useState } from "react";
+
 import { track } from "../../utils/tracking";
 import ThumbsDownIcon from "../svg/ThumbsDown";
 import ThumbsUpIcon from "../svg/ThumbsUp";
@@ -6,30 +8,39 @@ import ThumbsUpIcon from "../svg/ThumbsUp";
 import style from "./FeedbackCollector.module.scss";
 
 export function FeedbackCollector() {
+  const [gaveFeedback, setGaveFeedback] = useState(false);
+
   const handleFeedback = function (value: boolean) {
     const props = {
       title: document.querySelector(`h1`).textContent,
     }
     track(value ? `Docs Promoter` : `Docs Detractor`, props);
+    setGaveFeedback(true);
   };
 
   return (
     <>
-      <p className="nx-text-lg nx-font-medium">Was this page useful?</p>
-      <div className={style.buttonContainer}>
-        <button className={style.button} onClick={() => handleFeedback(true)}>
-          Yes
-          <span className={clsx(style.thumbsUp, style.inlineIcon)}>
-            <ThumbsUpIcon />
-          </span>
-        </button>
-        <button className={style.button} onClick={() => handleFeedback(false)}>
-          Could be better
-          <span className={clsx(style.thumbsDown, style.inlineIcon)}>
-            <ThumbsDownIcon />
-          </span>
-        </button>
-      </div>
+      {gaveFeedback ?
+        <p className="nx-text-lg nx-font-medium">Thanks for your feedback!</p> : (
+          <>
+            <p className="nx-text-lg nx-font-medium">Was this page useful?</p>
+            <div className={style.buttonContainer}>
+              <button className={style.button} onClick={() => handleFeedback(true)}>
+                Yes
+                <span className={clsx(style.thumbsUp, style.inlineIcon)}>
+                  <ThumbsUpIcon />
+                </span>
+              </button>
+              <button className={style.button} onClick={() => handleFeedback(false)}>
+                Could be better
+                <span className={clsx(style.thumbsDown, style.inlineIcon)}>
+                  <ThumbsDownIcon />
+                </span>
+              </button>
+            </div>
+          </>
+        )
+      }
     </>
   );
 }
