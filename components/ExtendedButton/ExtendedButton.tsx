5658d0f7c2749ebdce9872abe182c360f9c91bf6
Mike Murray <mike.murray@mixpanel.com>
2024-04-19 14:18:24 -0700

    Separate file for extended button

---
+++ components/ExtendedButton/ExtendedButton.tsx
@@ -0,0 +1,27 @@
+export default function ExtendedButton({ title, link }) {
+    const handleClick = () => {
+      window.location.href = link; // Redirects to the specified link
+    }
+  
+    return (
+      <button onClick={handleClick} class="text-white 
+      
+      // Background
+      bg-gradient-to-b from-purple50 to-purple100 hover:from-purple50 hover:to-purple100 active:from-purple50 active:to-purple140
+  
+      // Shadow
+      shadow-sm hover:shadow-md
+  
+      // Shadow
+      rounded-full hover:rounded-lg 
+      
+      // Font 
+      font-medium  text-md 
+      
+      // Padding
+      px-7 py-2.5">
+        {title}
+      </button>
+    )
+  }
+  
