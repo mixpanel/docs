746ce9df20bb5b323e963edf8b96121929081dbe
Mike Murray <mike.murray@mixpanel.com>
2024-04-25 15:57:24 -0700

    Add FAQs to the bottom of each step. Comment out Tips and Tricks for now.

---
+++ components/FAQBox/FAQBox.tsx
@@ -0,0 +1,13 @@
+export default function FAQBox({ title, children }) {
+    return (
+      <details
+        closed
+        className="last-of-type:mb-0 rounded-xl bg-[#f5f2f2] dark:bg-neutral-800 p-0 mt-6"
+      >
+        <summary>
+          <strong className="text-lg font-medium text-[#1b0b3b]">{title}</strong>
+        </summary>
+        <div className="nx-p-2">{children}</div>
+      </details>
+    )
+  }
