3cdb18707154e6936b443e0d9eccf2599c52ee6a
Tiffany Qi <tiffany19qi@gmail.com>
2024-05-22 15:06:58 -0700

    update console log

--- openapi/publish.js
+++ openapi/publish.js
@@ -56,7 +56,7 @@ async function updateSpecs() {
     const spec = YAML.parse(yamlStr);
     const specMeta = remoteSpecMetas.find((m) => m.title === spec.info.title);
     if (!specMeta) {
-      console.log(`!!! No spec found for "${spec.info.title}"`);
+      console.log(`!!! No spec found for "${spec.info.title}". Please upload it first via the command line as found in the developer runbook.`);
       continue;
     }
     const specId = specMeta.id;
