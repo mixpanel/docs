8a0f89fe74985a66133321c224e421abe0e157a4
Tiffany Qi <tiffany19qi@gmail.com>
2024-10-04 15:33:22 -0700

    get title in page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from 'next/router'
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import Search from "./components/Search/Search";
 import MixpanelLogoWordmark from "./components/svg/MixpanelLogoWordmark";
 import MainContent from "./components/MainContent/MainContent";
@@ -17,34 +18,47 @@ const config: DocsThemeConfig = {
     defaultTheme: `system`,
   },
   docsRepositoryBase: "https://github.com/mixpanel/docs/tree/main",
-  head: (
-    <>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href={`https://cdn.mxpnl.com/marketing-site/static/favicons/apple-touch-icon.png`}
-      />
-      <link
-        rel="icon"
-        sizes="16x16"
-        href="https://cdn.mxpnl.com/marketing-site/static/favicons/favicon-16x16.png"
-        type="image/png"
-      />
-      <link
-        rel="icon"
-        sizes="32x32"
-        href="https://cdn.mxpnl.com/marketing-site/static/favicons/favicon-32x32.png"
-        type="image/png"
-      />
-      <link
-        rel="mask-icon"
-        href={`https://cdn.mxpnl.com/marketing-site/static/favicons/safari-pinned-tab.svg`}
-        color="#7856ff"
-      />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-    </>
-  ),
+  head() {
+    const { asPath } = useRouter();
+    const { title } = useConfig();
+    const url = `https://docs.mixpanel.com${asPath}`;
+
+    return (
+      <>
+        <title>{title} - Mixpanel Docs</title>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={`${title} - Mixpanel Docs`} />
+        <meta
+          property="og:description"
+          content={`Read more about Mixpanel documentation`}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`https://cdn.mxpnl.com/marketing-site/static/favicons/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          sizes="16x16"
+          href="https://cdn.mxpnl.com/marketing-site/static/favicons/favicon-16x16.png"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          sizes="32x32"
+          href="https://cdn.mxpnl.com/marketing-site/static/favicons/favicon-32x32.png"
+          type="image/png"
+        />
+        <link
+          rel="mask-icon"
+          href={`https://cdn.mxpnl.com/marketing-site/static/favicons/safari-pinned-tab.svg`}
+          color="#7856ff"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </>
+    );
+  },
   feedback: {
     content: "Question? Contact our Support Team",
     useLink: () => "https://mixpanel.com/get-support",
@@ -55,9 +69,6 @@ const config: DocsThemeConfig = {
   logo: <MixpanelLogoWordmark width={125} />,
   logoLink: "https://mixpanel.com/home/",
   main: MainContent,
-  // useNextSeoProps: () => ({
-  //   titleTemplate: "%s - Mixpanel Docs",
-  // }),
   search: {
     component: Search,
   },
