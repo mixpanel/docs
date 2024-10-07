5998c996d0e95154c1f382f0de0581f2000d5e9a
Ryan Curtis <ryan.curtis@mixpanel.com>
2024-10-07 13:46:14 -0400

    address comments

--- components/SignUpButton/SignUpButton.tsx
+++ components/SignUpButton/SignUpButton.tsx
@@ -2,8 +2,8 @@ import style from "./SignUpButton.module.scss";
 
 export default function SignUpButton() {
   return (
-    <div className={style.signUpButton}>
-      <a href="https://mixpanel.com/register">Sign Up</a>
-    </div>
+    <a className={style.signUpButton} href="https://mixpanel.com/register">
+      Sign Up
+    </a>
   );
 }
