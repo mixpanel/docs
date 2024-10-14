21b256bf589de3b13da9aea2d3e9bf3fd84fc575
myronkaifung <97630035+myronkaifung@users.noreply.github.com>
2024-10-13 18:13:47 -0700

    Update _meta.ts

--- pages/docs/migration/_meta.ts
+++ pages/docs/migration/_meta.ts
@@ -1,6 +1,5 @@
 export default {
     "adobe-analytics": "Adobe Analytics",
     "amplitude": "Amplitude",
-    "google-analytics": "Google Analytics",
-    "flurry": "Flurry"
+    "google-analytics": "Google Analytics"
 }
