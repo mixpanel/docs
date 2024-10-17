b979092cd76477d64ee03d09c875064f673ec5fc
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-10-17 13:47:11 -0600

    Image component

---
+++ components/ImageFrame/ImageFrame.tsx
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import Image from "next/image";
+// https://www.tailwind-variants.org/docs
+import { tv } from "tailwind-variants";
+
+type ImageFrameProps = {
+  src: string;
+  alt?: string;
+};
+
+export default function ImageFrame({
+  alt = "Thumnail of screenshot",
+  ...props
+}: ImageFrameProps) {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const isTall = imageRef.current?.height > 400;
+
+  const imageFrame = tv({
+    base: "nx-aspect-video nx-overflow-hidden nx-nx-mt-8 lg:nx-rounded-3xl nx-roundex-xl nx-bg-base80 nx-bg-gradient-to-t nx-from-grey20 nx-mb-8 lg:nx-px-14",
+    variants: {
+      isTall: {
+        false: "nx-flex nx-justify-center nx-items-center",
+      },
+    },
+  });
+
+  const imageSelf = tv({
+    base: "nx-w-full max-h-96 h-full nx-shadow-sm",
+    variants: {
+      isTall: {
+        true: "nx-border nx-border-grey20",
+        false: "nx-rounded-md",
+      },
+    },
+  });
+
+  return (
+    <div className={imageFrame({ isTall: isTall })}>
+      <Image
+        ref={imageRef}
+        src={props.src}
+        height={imageRef.current?.height}
+        width={imageRef.current?.width}
+        className={imageSelf({ isTall: isTall })}
+        alt={alt}
+      />
+    </div>
+  );
+}
