94412385116e1d0ae08407f5c64a651868f56e0b
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-10-18 14:14:45 -0600

    Add next/image to introduce lazy loading and get/set img dimensions

--- components/ImageFrame/ImageFrame.tsx
+++ components/ImageFrame/ImageFrame.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import Image from "next/image";
 // https://www.tailwind-variants.org/docs
 import { tv } from "tailwind-variants";
@@ -13,7 +13,17 @@ export default function ImageFrame({
   ...props
 }: ImageFrameProps) {
   const imageRef = useRef<HTMLImageElement>(null);
-  const isTall = imageRef.current?.height > 400;
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    if (imageRef.current) {
+      setHeight(imageRef.current.getBoundingClientRect().height);
+      setWidth(imageRef.current.getBoundingClientRect().width);
+    }
+  }, []);
+
+  const isTall = height > 400;
 
   const imageFrame = tv({
     base: "nx-aspect-video nx-overflow-hidden nx-nx-mt-8 lg:nx-rounded-3xl nx-roundex-xl nx-bg-base80 nx-bg-gradient-to-t nx-from-grey20 nx-mb-8 lg:nx-px-14",
@@ -33,14 +43,15 @@ export default function ImageFrame({
       },
     },
   });
+  console.log(width);
 
   return (
     <div className={imageFrame({ isTall: isTall })}>
       <Image
         ref={imageRef}
         src={props.src}
-        height={imageRef.current?.height}
-        width={imageRef.current?.width}
+        height={height}
+        width={width}
         className={imageSelf({ isTall: isTall })}
         alt={alt}
       />
