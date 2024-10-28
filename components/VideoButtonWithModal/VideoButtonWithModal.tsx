1fd14a53e203344204685218e59fff2d60622d26
Ryan Curtis <ryan.curtis@mixpanel.com>
2024-10-28 11:30:30 -0400

    [Changelog] post page updates to use new formatting (#1574)
    * update page layout
    
    * add header component
    
    * add new components to post page
    '
    
    * update video button styling

--- components/VideoButtonWithModal/VideoButtonWithModal.tsx
+++ components/VideoButtonWithModal/VideoButtonWithModal.tsx
@@ -8,10 +8,12 @@ import VideoIcon from "../svg/VideoIcon";
 type VideoButtonModalProps = {
   src: string;
   title?: string;
+  showThumbnail?: boolean;
 };
 
 export default function VideoButtonWithModal({
   title = "Video about this feature",
+  showThumbnail = true,
   ...props
 }: VideoButtonModalProps) {
   const embedURL = getVideoEmbedURL(props.src);
@@ -20,17 +22,52 @@ export default function VideoButtonWithModal({
   // TODO: update this style and abstract it as time allows to a single button component
   // https://www.figma.com/design/8kiticjQNChvsP9y7s9SRf/Product-Releases-(Copy)?node-id=982-75355&node-type=frame&t=O7vwnwoAoOx42stw-0
   const playButton = tv({
-    base: "nx-flex nx-items-center nx-rounded-full nx-border-2 nx-border-purple140 nx-px-2 nx-py-1 nx-text-xs nx-font-semibold nx-text-purple140 nx-shadow-sm hover:nx-rounded-lg focus-visible:nx-outline focus-visible:nx-outline-2 focus-visible:nx-outline-offset-2 focus-visible:nx-outline-purple140",
+    base: "nx-flex nx-items-center nx-px-2 nx-py-1 nx-text-xs nx-font-semibold nx-text-purple140 nx-shadow-sm focus-visible:nx-outline focus-visible:nx-outline-2 focus-visible:nx-outline-offset-2 focus-visible:nx-outline-purple140",
+    variants: {
+      showThumbnail: {
+        true: "nx-border nx-border-base120 nx-rounded-2xl nx-px-4 nx-py-4 nx-relative",
+        false:
+          "nx-border-2 nx-border-purple140 nx-rounded-full hover:nx-rounded-lg",
+      },
+    },
+  });
+
+  const buttonWrapper = tv({
+    base: "",
+    variants: {
+      showThumbnail: {
+        true: "nx-bg-white nx-relative nx-z-0 nx-w-fit nx-rounded-2xl nx-mt-10 nx-mb-10",
+      },
+    },
   });
 
   return (
-    <>
+    <div className={buttonWrapper({ showThumbnail: showThumbnail })}>
       <button
         type="button"
-        className={playButton()}
+        className={playButton({ showThumbnail: showThumbnail })}
         onClick={() => setIsOpen(true)}
       >
-        <VideoIcon />
+        {showThumbnail ? (
+          <iframe
+            src={embedURL}
+            style={{
+              width: "170px",
+              aspectRatio: 16 / 9,
+              height: "auto",
+              borderRadius: "8px",
+              marginRight: "16px",
+              zIndex: "-1",
+            }}
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            title={title}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          ></iframe>
+        ) : (
+          <VideoIcon />
+        )}
         Watch the Video
       </button>
 
@@ -39,7 +76,7 @@ export default function VideoButtonWithModal({
         onClose={() => setIsOpen(false)}
         className="relative z-50"
       >
-        <div className="nx-fixed nx-inset-0 nx-flex nx-w-screen nx-items-center nx-justify-center nx-p-4 nx-bg-black nx-bg-opacity-80">
+        <div className="nx-fixed nx-inset-0 nx-flex nx-w-screen nx-items-center nx-justify-center nx-p-4 nx-bg-black nx-bg-opacity-80 nx-z-20">
           <Dialog.Panel className="nx-w-full nx-max-w-6xl">
             <iframe
               src={embedURL}
@@ -56,6 +93,6 @@ export default function VideoButtonWithModal({
           </Dialog.Panel>
         </div>
       </Dialog>
-    </>
+    </div>
   );
 }
