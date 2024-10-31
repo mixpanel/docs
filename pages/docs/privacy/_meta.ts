f4ea0c1271f18aa748e5133e8e41eaee4602539e
Jun Kim <jun.kim@mixpanel.com>
2024-10-31 11:47:14 -0400

    india residency update

--- pages/docs/privacy/_meta.ts
+++ pages/docs/privacy/_meta.ts
@@ -6,6 +6,7 @@ export default {
         "href": "https://mixpanel.com/legal/privacy-policy"
     },
     "gdpr-compliance": "GDPR Compliance",
+    "india-residency": "India Data Residency",
     "eu-residency": "EU Residency",
     "end-user-data-management": "End User Data Management"
 }
