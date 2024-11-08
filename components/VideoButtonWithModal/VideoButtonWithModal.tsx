df2edcc3404335b3cc2b0bc0f3ed7622995b37c7
Jun Kim <jun.kim@mixpanel.com>
2024-11-08 15:40:44 -0500

    add styling

--- components/VideoButtonWithModal/VideoButtonWithModal.tsx
+++ components/VideoButtonWithModal/VideoButtonWithModal.tsx
@@ -12,7 +12,8 @@ type VideoButtonModalProps = {
   showThumbnail?: boolean;
 };
 
-const PLAY_BUTTON_BASE_CLASS = "nx-flex nx-items-center nx-px-2 nx-py-1 nx-text-xs nx-font-semibold nx-text-purple140 nx-shadow-sm focus-visible:nx-outline focus-visible:nx-outline-2 focus-visible:nx-outline-offset-2 focus-visible:nx-outline-purple140";
+const PLAY_BUTTON_BASE_CLASS =
+  "nx-flex nx-items-center nx-px-2 nx-py-1 nx-text-xs nx-font-semibold nx-text-purple140 nx-shadow-sm focus-visible:nx-outline focus-visible:nx-outline-2 focus-visible:nx-outline-offset-2 focus-visible:nx-outline-purple140";
 
 export default function VideoButtonWithModal({
   title = "Video about this feature",
@@ -24,7 +25,9 @@ export default function VideoButtonWithModal({
 
   // TODO: update this style and abstract it as time allows to a single button component
   // https://www.figma.com/design/8kiticjQNChvsP9y7s9SRf/Product-Releases-(Copy)?node-id=982-75355&node-type=frame&t=O7vwnwoAoOx42stw-0
-  const playButtonClass = showThumbnail ? `thumbnailPlayButton ${PLAY_BUTTON_BASE_CLASS}` : `playButton ${PLAY_BUTTON_BASE_CLASS}`;
+  const playButtonClass = showThumbnail
+    ? `thumbnailPlayButton ${PLAY_BUTTON_BASE_CLASS}`
+    : `playButton ${PLAY_BUTTON_BASE_CLASS}`;
 
   const playButton = tv({
     base: playButtonClass,
