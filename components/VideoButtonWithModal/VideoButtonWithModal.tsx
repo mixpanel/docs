3c2c86cecdfc40df2462b078f0b1d0e20bc862a4
KyungJun Kim <jun.kim@mixpanel.com>
2024-11-12 09:20:19 -0500

    [ChangeLog] Add states to Watch the video button in post (#1606)
    * add styling
    
    * light style
    
    * pr feedback

--- components/VideoButtonWithModal/VideoButtonWithModal.tsx
+++ components/VideoButtonWithModal/VideoButtonWithModal.tsx
@@ -12,6 +12,8 @@ type VideoButtonModalProps = {
   showThumbnail?: boolean;
 };
 
+const PLAY_BUTTON_BASE_CLASS = "nx-flex nx-items-center nx-px-2 nx-py-1 nx-text-xs nx-font-semibold nx-text-purple140 nx-shadow-sm focus-visible:nx-outline focus-visible:nx-outline-2 focus-visible:nx-outline-offset-2 focus-visible:nx-outline-purple140";
+
 export default function VideoButtonWithModal({
   title = "Video about this feature",
   showThumbnail = true,
@@ -22,8 +24,10 @@ export default function VideoButtonWithModal({
 
   // TODO: update this style and abstract it as time allows to a single button component
   // https://www.figma.com/design/8kiticjQNChvsP9y7s9SRf/Product-Releases-(Copy)?node-id=982-75355&node-type=frame&t=O7vwnwoAoOx42stw-0
+  const playButtonClass = showThumbnail ? `thumbnailPlayButton ${PLAY_BUTTON_BASE_CLASS}` : `playButton ${PLAY_BUTTON_BASE_CLASS}`;
+
   const playButton = tv({
-    base: "playButton nx-flex nx-items-center nx-px-2 nx-py-1 nx-text-xs nx-font-semibold nx-text-purple140 nx-shadow-sm focus-visible:nx-outline focus-visible:nx-outline-2 focus-visible:nx-outline-offset-2 focus-visible:nx-outline-purple140",
+    base: playButtonClass,
     variants: {
       showThumbnail: {
         true: "nx-border nx-border-base120 nx-rounded-2xl nx-px-4 nx-py-4 nx-relative",
@@ -37,7 +41,7 @@ export default function VideoButtonWithModal({
     base: "",
     variants: {
       showThumbnail: {
-        true: "nx-bg-white nx-relative nx-z-0 nx-w-fit nx-rounded-2xl nx-mt-10 nx-mb-10",
+        true: "nx-relative nx-z-0 nx-w-fit nx-rounded-2xl nx-mt-10 nx-mb-10",
       },
     },
   });
@@ -68,12 +72,15 @@ export default function VideoButtonWithModal({
           ></iframe>
         ) : (
           <span className={"videoIconWrapper"}>
-            <span className={"videoIconLight"}> <VideoIcon /> </span>
-            <span className={"videoIconDark"}> <VideoIconDark /> </span>
+            <span className={"videoIconLight"}>
+              <VideoIcon />
+            </span>
+            <span className={"videoIconDark"}>
+              <VideoIconDark />
+            </span>
           </span>
-          
         )}
-        Watch the Video
+        {`Watch the video ${showThumbnail ? `→` : ``}`}
       </button>
 
       <Dialog
