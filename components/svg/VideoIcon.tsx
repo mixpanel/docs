5cf2870733c2174a21167bcde2c681b8575683aa
Ryan Curtis <ryan.curtis@mixpanel.com>
2024-11-19 13:36:58 -0500

    show video in feed if post doesn't have image (#1627)

--- components/svg/VideoIcon.tsx
+++ components/svg/VideoIcon.tsx
@@ -1,23 +1,64 @@
 export default function VideoIcon() {
   return (
-<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-  <g filter="url(#filter0_i_982_76833)">
-    <circle cx="10.8027" cy="10.3469" r="10" fill="#5028C0"/>
-  </g>
-  <circle cx="10.8027" cy="10.3469" r="10.0962" stroke="white" stroke-opacity="0.1" stroke-width="0.192307"/>
-  <path d="M15.5305 9.99026C15.8523 10.1194 15.8523 10.575 15.5305 10.7041L9.06317 13.2997C8.81047 13.4011 8.53531 13.215 8.53531 12.9427L8.53531 7.75165C8.53531 7.47936 8.81047 7.29329 9.06317 7.39471L15.5305 9.99026Z" fill="white"/>
-  <defs>
-    <filter id="filter0_i_982_76833" x="0.610352" y="-1.51213" width="20.3848" height="22.0514" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-      <feOffset dy="-1.66667"/>
-      <feGaussianBlur stdDeviation="2.5"/>
-      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-      <feColorMatrix type="matrix" values="0 0 0 0 0.313726 0 0 0 0 0.156863 0 0 0 0 0.752941 0 0 0 0.4 0"/>
-      <feBlend mode="normal" in2="shape" result="effect1_innerShadow_982_76833"/>
-    </filter>
-  </defs>
-</svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+    >
+      <g filter="url(#filter0_i_982_76833)">
+        <circle cx="10.8027" cy="10.3469" r="10" fill="#5028C0" />
+      </g>
+      <circle
+        cx="10.8027"
+        cy="10.3469"
+        r="10.0962"
+        stroke="white"
+        strokeOpacity="0.1"
+        strokeWidth="0.192307"
+      />
+      <path
+        d="M15.5305 9.99026C15.8523 10.1194 15.8523 10.575 15.5305 10.7041L9.06317 13.2997C8.81047 13.4011 8.53531 13.215 8.53531 12.9427L8.53531 7.75165C8.53531 7.47936 8.81047 7.29329 9.06317 7.39471L15.5305 9.99026Z"
+        fill="white"
+      />
+      <defs>
+        <filter
+          id="filter0_i_982_76833"
+          x="0.610352"
+          y="-1.51213"
+          width="20.3848"
+          height="22.0514"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-1.66667" />
+          <feGaussianBlur stdDeviation="2.5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.313726 0 0 0 0 0.156863 0 0 0 0 0.752941 0 0 0 0.4 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_982_76833"
+          />
+        </filter>
+      </defs>
+    </svg>
   );
 }
