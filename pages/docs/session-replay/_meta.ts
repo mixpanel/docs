139df686d125d298cabbd2f77c604ec6ab597800
myronkaifung <97630035+myronkaifung@users.noreply.github.com>
2025-01-08 09:25:01 -0800

    Update _meta.ts

--- pages/docs/session-replay/_meta.ts
+++ pages/docs/session-replay/_meta.ts
@@ -1,5 +1,5 @@
 export default {
     "session-replay-web": "Web",
-    "session-replay-ios": "iOS (Alpha)"
+    "session-replay-ios": "iOS (Alpha)",
     "session-replay-privacy-controls": "Privacy Controls"
 }
