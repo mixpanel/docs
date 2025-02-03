aa624f599af730e463d80aedbf98afae28c82acc
Abby Braiman <abby.braiman@mixpanel.com>
2025-02-03 10:27:55 -0800

    Update _meta.ts hierarchy in tracking methods

--- pages/docs/tracking-methods/_meta.ts
+++ pages/docs/tracking-methods/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "choosing-the-right-method": "Choosing the Right Method",
     "sdks": "SDKs",
+    "autocapture": "Autocapture",
     "warehouse-connectors": "Warehouse Connectors",
     "integrations": "Integrations",
     "id-management": "ID Management"
