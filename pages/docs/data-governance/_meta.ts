1c393bd157a52ec4396bca59180c6713f224828b
djmixpanel <122127729+djmixpanel@users.noreply.github.com>
2025-03-21 16:40:14 -0400

    Dj gov items reorder (#1803)
    * Update _meta.ts
    
    reordering gov features to be more chronological by event lifecycle
    
    * Update data-governance.mdx
    
    ---------
    
    Co-authored-by: myronkaifung <97630035+myronkaifung@users.noreply.github.com>

--- pages/docs/data-governance/_meta.ts
+++ pages/docs/data-governance/_meta.ts
@@ -1,8 +1,8 @@
 export default {
     "lexicon": "Lexicon",
-    "data-views-and-classification": "Data Views & Classification",
-    "event-approval": "Event Approval",
-    "data-volume-monitoring": "Data Volume Monitoring",
-    "data-standards": "Data Standards",
+    "data-views-and-classification": "Data Views & Classification",    
+    "data-standards": "Data Standards",    
+    "event-approval": "Event Approval",        
+    "data-volume-monitoring": "Data Volume Monitoring",    
     "data-clean-up": "Data Clean-Up"
 }
