3675a486b2fafdaae1b2f3ef3faea51525335ebd
myronkaifung <97630035+myronkaifung@users.noreply.github.com>
2025-04-24 11:35:01 -0700

    Myronkaifung sessionreplay reorg (#1830)
    * new pages and index
    
    * top-level SR page (how to use)
    
    * add FAQ to top-level page
    
    * SR developer guide Web
    
    * SR developer guide iOS
    
    * SR developer guide android
    
    * SR declutter JS SDK page
    
    * add getReplayId methods for mobile
    
    * add meta for nested SDK pages
    
    * SR iOS SDK reference
    
    * add SR android SDK
    
    * update URLs and privacy tabbing
    
    * heatmaps
    
    * update privacy page name
    
    * heatmap images
    
    * fix conflict
    
    * heatmaps in JS SDK page
    
    * remove tabs from privacy page
    
    * reference to SR in SDK page
    
    * redirects
    
    * edit Android banner
    
    * small typo
    
    ---------
    
    Co-authored-by: Myron Fung <myronfung@MyronFung---MP-MBP.local>

--- pages/docs/session-replay/_meta.ts
+++ pages/docs/session-replay/_meta.ts
@@ -1,6 +1,5 @@
 export default {
-    "session-replay-web": "Web",
-    "session-replay-ios": "iOS (Beta)",
-    "session-replay-android": "Android (Alpha)",
-    "session-replay-privacy-controls": "Privacy Controls"
+    "implement-session-replay": "Implement Session Replay",
+    "session-replay-privacy-controls": "Session Replay Privacy Controls",
+    "heatmaps": "Heatmaps (beta)"
 }
---
+++ pages/docs/tracking-methods/sdks/android/_meta.ts
@@ -0,0 +1,3 @@
+export default {
+    "android-replay": "Session Replay (Android)"
+}
