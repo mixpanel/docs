8c8ae1c2e79831187a4adb0c0c47ef307793f3b1
Tiffany Qi <tiffany19qi@gmail.com>
2025-09-15 17:47:14 -0700

    move layout and mdx to app

--- app/mdx-components.tsx
+++ app/mdx-components.tsx
@@ -1,7 +1,7 @@
 import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
 
-import ChangelogPostHeader from "./components/ChangelogPostHeader/ChangelogPostHeader";
-import { VideoButtonWithModal } from "./components/VideoButtonWithModal";
+import ChangelogPostHeader from "../components/ChangelogPostHeader/ChangelogPostHeader";
+import { VideoButtonWithModal } from "../components/VideoButtonWithModal";
 
 const docsComponents = getDocsMDXComponents()
  
