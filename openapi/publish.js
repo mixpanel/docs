35cfe185c748671306dbdbb06157a6e825f4e589
Tiffany Qi <tiffany19qi@gmail.com>
2025-09-30 15:01:09 -0700

    remove conversion step

--- openapi/publish.js
+++ openapi/publish.js
@@ -4,7 +4,6 @@ const process = require("node:process");
 const util = require("node:util");
 const YAML = require("yaml");
 
-const env = { ...process.env, DEBUG: "rdme*" };
 const execFile = util.promisify(require("node:child_process").execFile);
 
 const README_API_KEY = process.env.README_API_KEY;
@@ -29,21 +28,6 @@ async function execAndLog(cmd, args) {
   }
 }
 
-function getSlugFromJson(file) {
-  const splitFile = file.split(`.json`);
-  return splitFile[0];
-}
-
-function getSlugFromYaml(file) {
-  const splitFile = file.split(`.yaml`);
-  return splitFile[0];
-}
-
-function convertSlugToJson(file) {
-  const slug = getSlugFromYaml(file);
-  return `${slug}.json`;
-}
-
 async function updateSpecs() {
   // fetch IDs of openapi specs via readme API
   const res = await fetch(
@@ -60,42 +44,30 @@ async function updateSpecs() {
   const outBase = path.resolve(__dirname, `out`);
   const filenames = fs
     .readdirSync(outBase)
-    .filter((fn) => fn.endsWith(`.yaml`));
+    .filter((fn) => fn.endsWith(`.json`));
 
   if (!remoteSpecMetas) {
     console.error(`!!! No remote specs found, please double check the API`);
     process.exit(1);
   }
 
-  for (specFile of filenames) {
+  for (specFile of [filenames[0]]) {
     // get ID of each spec by matching title between filename and metadata
     const fullPath = path.join(outBase, specFile);
     const yamlStr = fs.readFileSync(fullPath, "utf8");
     const spec = YAML.parse(yamlStr);
-    const specMeta = remoteSpecMetas.data.find((m) => getSlugFromJson(m.filename) === getSlugFromYaml(specFile));
+    const specMeta = remoteSpecMetas.data.find((m) => m.filename === specFile);
     if (!specMeta) {
       console.log(`!!! No spec found for "${spec.info.title}". Please upload it as found in the developer.mixpanel.com runbook.`);
       continue;
     }
 
     // validate and publish spec
-    console.log(`Updating ${spec.info.title} (${specFile}`);
+    console.log(`Updating ${spec.info.title} (${specFile})`);
     await execAndLog('npx', ['rdme@10.5.1', 'openapi:validate', fullPath]);
-    // this converts the file to json, since it does this step directly in readme anyway
-    await execAndLog(
-      'npx', ['rdme@10.5.1', 'openapi', 'convert', fullPath, `--out=${convertSlugToJson(fullPath)}`], {env}
-    );
     // publish the json version
     await execAndLog(
-      'npx', [
-        'rdme@10.5.1',
-        'openapi',
-        'upload',
-        `${convertSlugToJson(fullPath)}`,
-        `--key=${README_API_KEY}`,
-        `--slug=${convertSlugToJson(specFile)}`,
-        `--confirm-overwrite`
-      ], {env}
+      'npx', ['rdme@10.5.1', 'openapi', 'upload', `${fullPath}`, `--key=${README_API_KEY}`, `--slug=${specFile}`, `--confirm-overwrite`],
     );
   }
 }
