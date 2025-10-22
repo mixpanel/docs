5830a3235a74e275af12e075fb6bd7bfff60ae73
Ryan Curtis <176308989+ryancurtis1@users.noreply.github.com>
2025-10-22 20:52:12 +0000

    simplify formatting

--- pages/docs/quickstart/_meta.ts
+++ pages/docs/quickstart/_meta.ts
@@ -2,7 +2,7 @@ export default {
   "install-mixpanel": "Install Mixpanel",
   "identify-users": "Identify Users",
   "capture-events": "Capture Events",
-  "install-with-ai": "Install with AI [beta]",
+  "install-with-ai": "Install with AI [BETA]",
   "tips-and-tricks": {
     "display": "hidden"
   },
