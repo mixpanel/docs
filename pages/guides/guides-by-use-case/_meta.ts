4a34858f7ae46e77f57a38df24bc679a2be3b342
Myron Fung <myron.fung@mixpanel.com>
2025-10-24 15:57:06 -0700

    rename driving product innovation pages keeping same path

--- pages/guides/guides-by-use-case/_meta.ts
+++ pages/guides/guides-by-use-case/_meta.ts
@@ -1,5 +1,5 @@
 export default {
     "build-a-tracking-strategy": "How to Build a Tracking Strategy",
     "ensuring-data-quality": "Ensuring Data Quality",
-    "driving-product-innovation": "Driving Product Innovation"
+    "driving-product-innovation": "Drive Product Innovation"
 }
