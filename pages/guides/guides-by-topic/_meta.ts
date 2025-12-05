1f1009e18d6fe9cda1b0b57a3955e951b0e1b7c2
amymadden-bit <amy.madden@mixpanel.com>
2025-12-05 11:13:46 -0600

    Update _meta.ts

--- pages/guides/guides-by-topic/_meta.ts
+++ pages/guides/guides-by-topic/_meta.ts
@@ -1,6 +1,6 @@
 export default {
     "continuous-innovation": "Continuous Innovation",
-    "govern-data": "Long-Term Data Governance"
+    "govern-data": "Long-Term Data Governance",
     "features": "Features",
     "core-reports": "Core Reports"
 }
