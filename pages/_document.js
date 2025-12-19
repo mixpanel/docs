7c89013a0f3cd02f77aba7287e4286c4bd3a3b5d
Gabriel Vigil <gabrielvigil@q95h042m26-gabrielvigil.corp.mixpanel.com>
2025-12-19 14:25:57 -0700

    added all needed code to document instead of relying on gtm

--- pages/_document.js
+++ pages/_document.js
@@ -49,6 +49,7 @@ export default function Document() {
         </script>
         {/* Empty script tag as chrome bug fix, see https://stackoverflow.com/a/42969608/943337 */}
         <script> </script>
+        <div id="consent-banner"></div>
       </body>
     </Html>
   );
