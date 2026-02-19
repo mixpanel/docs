7985b60747fb51abda9236d7340c4e9086cf41d6
Howard Chen <122341946+howardchen11@users.noreply.github.com>
2026-02-19 20:06:58 +0000

    private beta

--- pages/docs/data-pipelines/integrations/_meta.ts
+++ pages/docs/data-pipelines/integrations/_meta.ts
@@ -2,7 +2,7 @@ export default {
   "aws-s3": "AWS S3",
   "azure-blob-storage": "Azure Blob Storage",
   "bigquery": "BigQuery",
-  "databricks": "Databricks",
+  "databricks": "Databricks (Private Beta)",
   "gcp-gcs": "Google Cloud Storage",
   "redshift-spectrum": "Redshift Spectrum",
   "snowflake": "Snowflake"
