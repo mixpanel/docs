1ae7febca42603ac3fccab175d699e306fb10d27
Sharan Multani <1847452+smultani@users.noreply.github.com>
2026-03-14 07:50:28 -0400

    Adding Extended Accordion
    - better search
    - copy icon
    - better ux

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,6 +7,7 @@ import MainContent from "./components/MainContent/MainContent";
 import SignUpButton from "./components/SignUpButton/SignUpButton";
 import ChangelogPostHeader from "./components/ChangelogPostHeader/ChangelogPostHeader";
 import { VideoButtonWithModal } from "./components/VideoButtonWithModal";
+import ExtendedAccordion from "./components/ExtendedAccordion/ExtendedAccordion";
 
 const config: DocsThemeConfig = {
   darkMode: true,
@@ -69,6 +70,7 @@ const config: DocsThemeConfig = {
   components: {
     ChangelogPostHeader: ChangelogPostHeader,
     VideoButtonWithModal: VideoButtonWithModal,
+    ExtendedAccordion: ExtendedAccordion,
   },
   search: {
     component: Search,
