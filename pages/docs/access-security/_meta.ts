09b6153b1b81e20f8415b411c4f841c94c66dad9
mherrman <mikey.herrman@mixpanel.com>
2026-03-31 15:55:44 -0700

    Rename 'audit-logs' to 'audit-log'

--- pages/docs/access-security/_meta.ts
+++ pages/docs/access-security/_meta.ts
@@ -2,5 +2,5 @@ export default {
     "login-methods": "Login Methods",
     "two-factor-authentication": "Two-Factor Authentication",
     "single-sign-on": "Single Sign-On",
-    "audit-logs": "Audit Logs"
+    "audit-log": "Audit Log"
 }
