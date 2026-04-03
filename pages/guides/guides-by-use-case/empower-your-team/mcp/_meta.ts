93f62c5c16196b1711ce57537188c7895bc833a2
amymadden-bit <amy.madden@mixpanel.com>
2026-04-03 09:39:58 -0500

    Create _meta.ts

---
+++ pages/guides/guides-by-use-case/empower-your-team/mcp/_meta.ts
@@ -0,0 +1,9 @@
+export default {
+    "ai": "AI",
+    "b2bsaas": "B2B Saas",
+    "Ecommerce": "Ecommerce",
+    "finance": "Finance",
+    "healthcare": "Healthcare",
+    "media": "Media & Entertainment"
+
+}
