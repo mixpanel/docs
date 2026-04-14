a111fd75032c7a31ade373bf902ce6348b89bbdd
amymadden-bit <amy.madden@mixpanel.com>
2026-04-14 13:43:25 -0500

    Update _meta.ts

--- pages/guides/guides-by-workflow/_meta.ts
+++ pages/guides/guides-by-workflow/_meta.ts
@@ -1,6 +1,6 @@
 export default {
     "build-tracking-strategy": "Build Tracking Strategy",
-    "embed-data": "Embed Data in Development Cycle"
+    "embed-data": "Embed Data in Development Cycle",
     "ensure-data-quality": "Ensure Data Quality",
     "data-privacy": "Manage PII & Data Privacy",
     "sync-with-mirror": "Sync with Mirror"
