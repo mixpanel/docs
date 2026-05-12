32a22d0077496f34cdfe12e94db22c9d169ef741
Argenis Jesus Ferrer Mora <argenisf@gmail.com>
2026-05-12 16:37:51 +0200

    updating the meta

--- pages/guides/guides-by-use-case/empower-your-team/_meta.ts
+++ pages/guides/guides-by-use-case/empower-your-team/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-    "use-mixpanel-agent": "Use Mixpanel Agent",
+    "mixpanel-agent": "Use Mixpanel Agent",
     "mcp": "Use Mixpanel MCP",
     "close-strategy-execution-gap": "Close Strategy-Execution Gap",
     "see-replays": "See Replays"
